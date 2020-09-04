39425fcab6a1f96a0f8456cdca8982f44c58b7e6
aresyang <yweiyun@163.com>
2020-09-04 20:34:58 +0800

    add benchmark

---
+++ script/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function build() {
+    deploy=$1
+    cd $deploy
+    git submodule update --init --recursive
+    cd -
+    mkdir $deploy/build
+    cd $deploy/build
+    cmake3 -DCMAKE_BUILD_TYPE=Release ..
+    make -j3 && make
+    cd -
+}
+
+#build  cpp-serialization-benchmark
+build format-benchmark
